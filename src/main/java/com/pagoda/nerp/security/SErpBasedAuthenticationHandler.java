be148a0587c056e7684d1740f55dca8ec98b1528
scutxieyun <scutxieyun@hotmail.com>
2016-10-14 08:36:44 +0800

    integrate authentication with serp

---
+++ src/main/java/com/pagoda/nerp/security/SErpBasedAuthenticationHandler.java
@@ -0,0 +1,55 @@
+package com.pagoda.nerp.security;
+
+import java.security.GeneralSecurityException;
+
+import javax.security.auth.login.FailedLoginException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.commons.lang3.StringUtils;
+import org.jasig.cas.adaptors.jdbc.AbstractJdbcUsernamePasswordAuthenticationHandler;
+import org.jasig.cas.authentication.HandlerResult;
+import org.jasig.cas.authentication.PreventedException;
+import org.jasig.cas.authentication.UsernamePasswordCredential;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Component;
+
+
+
+@Component("serpBaseAuthenticationHandler")
+public class SErpBasedAuthenticationHandler extends AbstractJdbcUsernamePasswordAuthenticationHandler implements InitializingBean{
+	
+	static String sql_template = "select count(*) from SAM_USERS where USERCODE = '%1s' and sys_encrypt_des3('%2s',1) = PASSWORD";
+	//static String sql_test = "select count(*) from SAM_USERS";
+	protected final transient Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Override
+    protected final HandlerResult authenticateUsernamePasswordInternal(final UsernamePasswordCredential credential)
+            throws GeneralSecurityException, PreventedException {
+
+		
+        if (StringUtils.isBlank(sql_template) || getJdbcTemplate() == null) {
+            throw new GeneralSecurityException("Authentication handler is not configured correctly");
+        }
+        logger.warn("ERPBase Authentication kick off");
+        final String username = credential.getUsername();
+        final String password = getPasswordEncoder().encode(credential.getPassword());
+        final int count;
+        final String sql = String.format(sql_template, username,password);
+        try {
+        	count = getJdbcTemplate().queryForObject(sql, Integer.class);
+        } catch (final DataAccessException e) {
+            throw new PreventedException("SQL exception while executing query for " + sql, e);
+        }
+        logger.warn("get count value {}",count);
+        if (count == 0) {
+            throw new FailedLoginException(username + " not found with SQL query.");
+        }
+        return createHandlerResult(credential, this.principalFactory.createPrincipal(username), null);
+    }
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+}
